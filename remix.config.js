67e34fc9051e2328368dfe078eec0e5e60ff8a4c
Bryan Eaton <bryaneaton13@gmail.com>
2022-05-17 12:19:40 -0400

    Update remix config.

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,12 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  appDirectory: "app",
-  browserBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8002
+  serverBuildTarget: 'netlify',
+  server: './server.js',
+  ignoredRouteFiles: ['**/.*'],
+  // appDirectory: "app",
+  // browserBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildDirectory: "build",
+  // devServerPort: 8002
 };
